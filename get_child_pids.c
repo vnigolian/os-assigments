ab81e159011b3bde81c0b6fa83aad656c786e4bc
oslab <oslab@oslab.epfl.ch>
2016-03-15 14:00:01 +0100

    first version of get_child_pids.c

--- get_child_pids.c
+++ get_child_pids.c
@@ -1,9 +1,40 @@
-#include<linux/linkage.h>
+#include <linux/linkage.h>
 #include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/spinlock.h>
+#include <asm/uaccess.h>
 
 asmlinkage long sys_get_child_pids(pid_t* list, size_t limit, size_t* num_children){
-	printk("pointer value : %p\n",list);
-	printk("limit : %u\n",limit);
-	printk("num_children : %u\n",*num_children);	
-	return 1;
+	if(!access_ok(pid_t*,list, sizeof(pid_t)) 
+	|| !access_ok(size_t*, num_children, sizeof(num_children))){
+		return -EFAULT;
+	}else if(list == NULL && !limit == 0){
+		return -EFAULT;
+	}else{
+		pid_t* pPids;
+		size_t num_child = 0;
+		
+		task_struct* p_childptr = current-> p_cptr;
+		size_t i = 0;
+		while(p_childptr != NULL){
+			*(pPids + num_child) = p_childptr->pid;
+			p_childptr = p_childptr->p_osptr;
+			num_child++;
+		}
+		
+		put_user(pNum,num_child);
+		
+		if(limit >= num_child){
+			for(i=0;i<num_child;i++){
+				put_user(*(pPids + i), list + i);
+			}
+		return 0;
+		}else{
+			for(i=0;i<limit;i++){
+				put_user(*(pPids + i), list + i);
+			}
+		return -ENOBUFS;
+		}
+	}
 }
+
--- get_child_pids_test.c
+++ get_child_pids_test.c
@@ -8,6 +8,17 @@ int main(){
 	size_t* num_children = &limit;
 	res = get_child_pids(list,limit,num_children);
 	printf("Get child pids return value : %ld\n",res);
+	printf("List of child processes : \n");
+	int i = 0;
+	if(*num_children < limit){
+		for(i=0; i< *num_children; i++){
+			printf("pid : %u\n",list[i]);
+		}
+	}else{
+		for(i=0; i< limit; i++){
+			printf("pid : %u\n",list[i]);
+		}
+	}
 
 	return 0;
 }
